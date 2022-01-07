83b4f5a6dcd97e0229e3caf9fcdfb83a00d9beec
Techead24 <74743818+Techead24@users.noreply.github.com>
2022-01-07 11:14:42 +0000

    Update Bag.test.js

--- JS-TS/Bag.test.js
+++ JS-TS/Bag.test.js
@@ -10,5 +10,14 @@ describe('defines a suite of tests for the Bag class', function () {
         // then
         expect(mandysBag.weight).toBe(13);
     });
+    
+    test('meets the weight requirements', function () {
+        // given
+        const mandysBag = new Bag(25);
+
+        // then
+        expect(mandysBag.isOverLimit()).toBe(true);
+    });
+
 
 });
